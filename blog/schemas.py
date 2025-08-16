71fed6ed65deca080cc72cded92cd67692c4197d
Sasmita  Rout <sasmita.rout@accionlabs.com>
2025-08-16 22:56:58 +0530

    Initial commit - FastAPI Blog API

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Blog(BaseModel):
     title: str
@@ -52,5 +53,13 @@ class User(BaseModel):
 class Login(BaseModel):
     username: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
 
     
--- blog/token.py
+++ blog/token.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta
 import jwt
+from jose import JWTError
+from blog.schemas import TokenData
+
 
 
 SECRET_KEY="5656565656ssssssssss56565656"
@@ -13,3 +16,14 @@ def create_access_token(data: dict):
     encoded_jwt =jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+def verify_token(token: str,credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data=TokenData(email=email)
+
+    except JWTError:
+        raise credentials_exception
